89f9fec6ca956d9963e39889cbf45ffdc34d8ece
jclee <jclee@atoncorp.com>
2020-05-28 19:58:29 +0900

    템플릿 변경

--- src/main/java/com/wincert/framework/controller/TestController.java
+++ src/main/java/com/wincert/framework/controller/TestController.java
@@ -1,42 +1,47 @@
 package com.wincert.framework.controller;
 
-import com.wincert.framework.dao.user.dto.UserInfoDto;
-import com.wincert.framework.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.List;
 
 @RequestMapping(value = {"/", ""})
 @Controller
 public class TestController {
 
-    @Autowired
-    UserService userService;
-
-    @RequestMapping(value = {"/", ""})
-    public String sayHello(Model model) {
-        System.out.println();
-        System.out.println("로그 테스트");
-
-        return "/dash/home";
+    @RequestMapping(value = {"/index"})
+    public String index() {
+        return "/seem/index";
     }
 
-    @RequestMapping("/user/userInfo")
-    public String userInfo(Model model) {
-        model.addAttribute("userName", "아토 인터넷");
-        model.addAttribute("userEmail", "admin@wincert.com");
-
-        return "/user/userInfo";
+    @RequestMapping(value = {"/index_step01"})
+    public String index_step01() {
+        return "/seem/index_step01";
     }
 
-    @RequestMapping("/user/all")
-    public String allUserInfo(Model model) {
-        List<UserInfoDto> users = userService.getAllUser();
-
-        model.addAttribute("users", users);
-        return "/user/userList";
+    @RequestMapping(value = {"/index_step02"})
+    public String index_step02() {
+        return "/seem/index_step02";
     }
+
+    @RequestMapping(value = {"/index_step03"})
+    public String index_step03() {
+        return "/seem/index_step03";
+    }
+
+    @RequestMapping(value = {"/index_step03_exam"})
+    public String index_step03_exam() {
+        return "/seem/index_step03_exam";
+    }
+
+    @RequestMapping(value = {"/index_step04"})
+    public String index_step04() {
+        return "/seem/index_step04";
+    }
+
+    @RequestMapping(value = {"/login"})
+    public String login() {
+        return "/seem/login";
+    }
+
+
 }
